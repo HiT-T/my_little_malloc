c3fe88742637eaaa343aac4490559b192476d129
HiT_Hlin <2811703252@qq.com>
2025-02-15 21:37:19 +0000

    implement mymalloc & initialize

---
+++ memtest.c
@@ -0,0 +1,80 @@
+/*A good way to organize your testing strategy is to(1) specify
+ *the requirements your library must satisfy, (2) describe how you could determine whether the
+ *requirements have been violated, and (3) write programs to check those conditions.
+ *For example :
+ *1. Requirement : malloc() reserves unallocated memory.
+ *2. Detection method : When successful, malloc() returns a pointer to an object that does not
+ *overlap with any other allocated object.
+ *3. Test : Write a program that allocates several large objects.Once allocation is complete, it fills
+ *each object with a distinct byte pattern(e.g., the first object is filled with 1, the second with
+ *	2, etc.).Finally, it checks whether each object still contains the written pattern. (That is,
+ *		writing to one object did not overwrite any other.)
+ *	Other properties you should test include :
+ * free() deallocates memory
+ * malloc() and free() arrange so that adjacent free blocks are coalesced
+ * The errors described in section 2 are detected and reported
+ * Leaked objects are detected and reported
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <string.h>
+ 
+ 
+ // Compile with -DREALMALLOC to use the real malloc() instead of mymalloc()
+ #ifndef REALMALLOC
+ #include "mymalloc.h"
+ #endif
+ 
+ // Compile with -DLEAK to leak memory
+ #ifndef LEAK
+ #define LEAK 0
+ #endif
+ 
+ #define MEMSIZE 4096
+ #define HEADERSIZE 8
+ #define OBJECTS 64
+ #define OBJSIZE (MEMSIZE / OBJECTS - HEADERSIZE)
+ 
+ int main(int argc, char **argv)
+ {
+     char *obj[OBJECTS];
+     int i, j, errors = 0;
+     
+     // fill memory with objects
+     for (i = 0; i < OBJECTS; i++) {
+         obj[i] = malloc(OBJSIZE);
+         if (obj[i] == NULL) {
+             printf("Unable to allocate object %d\n", i);
+             exit(1);
+         }
+     }
+     
+     // fill each object with distinct bytes
+     for (i = 0; i < OBJECTS; i++) {
+         memset(obj[i], i, OBJSIZE);
+     }
+     
+     // check that all objects contain the correct bytes
+     for (i = 0; i < OBJECTS; i++) {
+         for (j = 0; j < OBJSIZE; j++) {
+             if (obj[i][j] != i) {
+                 errors++;
+                 printf("Object %d byte %d incorrect: %d\n", i, j, obj[i][j]);
+             }
+         }
+     }
+ 
+     // free all objects
+     if (!LEAK) {
+         for (i = 0; i < OBJECTS; i++) {
+         free(obj[i]);
+         }
+     }
+     
+     printf("%d incorrect bytes\n", errors);
+     
+     return EXIT_SUCCESS;
+ }
+ 
--- mymalloc.c
+++ mymalloc.c
@@ -1,5 +1,3 @@
-#include <cstddef>
-
 #define MEMLENGTH 4096
 
 // 'static' prevent client code from accessing storage directly.
@@ -13,51 +11,84 @@ static union {
 } heap; 
 
 struct node {
-	int size;
+	int payload_size;
 	int allocated;
 };
 
-struct node* first_header = (struct node*)heap.bytes;
+struct node *first_header = (struct node *) heap.bytes;
 
 static int not_initialized = 1;
 
 static void leak_detection() {
-
-	// print out the number of leaked (allocated but unfree) chunks in heap.
+	//TODO print out the number of leaked (allocated but unfree) chunks in heap.
 }
 
 static void initialize() {
 	atexit(leak_detection);
-
 	
+	memset(first_header + 8, 0xAA, MEMLENGTH - 8);
+	first_header->payload_size = MEMLENGTH;
+	first_header->allocated = 0;
+	not_initialized = 0;
 }
 
-void * mymalloc(size_t size, char* file, int line) {
+//TODO figure out why using size_t data type.
+void * mymalloc(size_t size, char *file, int line) {
 	if (not_initialized) {
 		initialize();
 	}
 
-	// error detection
+	//TODO other error detections.
 
-	for (struct node* current_header = first_header; current_header < heap.bytes + MEMLENGTH; current_header += current_header->size) {
-		if (current_header >= size) {
-			// ���С
-			return (current_header + 8);
+	size_t real_size = (size + 7) & ~7;
+
+	for (struct node *curr_header = first_header; curr_header < first_header + MEMLENGTH;
+		 curr_header += 8 + curr_header->payload_size)
+	{
+		int *curr_size = &(curr_header->payload_size);
+
+		if (*curr_size >= real_size) {
+			if (*curr_size >= real_size + 16) {
+				struct node *new_header = (struct node *) curr_header + 8 + real_size;
+				new_header->payload_size = *curr_size - real_size - 8;
+				new_header->allocated = 0;
+				*curr_size = real_size;
+			} else {
+				// doesn't change payload_size cuz don't have space to allocate a new chunk.
+			}
+
+			curr_header->allocated = 1;
+
+			return (void *) (curr_header + 8);
 		} 
-		else if (current_header) {
-			// С�۴�
-		}
-		else {
-			printf("Unable to allocate %d bytes (%s %d)", size, file, line);
-			exit(2);
+		else if (*curr_size < real_size) {
+			for (struct node *next_header = curr_header + 8 + *curr_size; next_header < first_header + MEMLENGTH 
+				&& next_header->allocated == 0; next_header = next_header + 8 + (next_header->payload_size)) 
+			{ *curr_size += 8 + (next_header->payload_size); }
+
+			memset(curr_header + 8, 0xAA, *curr_size);
+			
+			if (*curr_size < real_size) { continue; }
+			else if (*curr_size >= real_size + 16) {
+				struct node *new_header = (struct node *) curr_header + 8 + real_size;
+				new_header->payload_size = *curr_size - real_size - 8;
+				new_header->allocated = 0;
+				*curr_size = real_size;
+			} else {
+				// doesn't change payload_size cuz don't have space to allocate a new chunk.
+			}
+
+			curr_header->allocated = 1;
+
+			return (void *) (curr_header + 8)
 		}
 	}
 
-	// (size + 7) & 7 instead of size + (8 - size % 8) to accelerate the code.
-
+	printf("Unable to allocate %d bytes (%s %d)", size, file, line);
+	exit(2);
 }
 
-void   myfree(void* ptr, char* file, int line) {
+void   myfree(void *ptr, char *file, int line) {
 	if (not_initialized) {
 		initialize();
 	}
